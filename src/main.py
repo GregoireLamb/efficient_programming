aec4fb8ef01ed377f5fbbe3d7f70bd5ec432a806
GregoireLamb <gdelambertye03@gmail.com>
2023-12-07 18:35:21 +0100

    parallel v1

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 from merge_sort import merge_sort
+from src.benchmark_analyzer import BenchmarkAnalyzer
+from src.parallel import Parallel
 
 
 def main():
     test_list = [21, 22, 23, 1, 5, 6, 7, 2, 3, 15, 12, 13, 14, 16, 17, 18, 19, 4, 8, 9, 10, 11, 20, 24, 25]
-    print("Unsorted list: ", test_list)
-    sorted_list = merge_sort(test_list)
-    print("Sorted list: ", sorted_list)
+    # print("Unsorted list: ", test_list)
+    # sorted_list = merge_sort(test_list)
+    # print("Sorted list: ", sorted_list)
+    #
+    # assert sorted(test_list) == sorted_list, f'Error in merge sort. Result is not sorted'
 
-    assert sorted(test_list) == sorted_list, f'Error in merge sort. Result is not sorted'
-
+    p = Parallel(4)
+    BenchmarkAnalyzer.run_benchmark(p.para_merge_sort(test_list))
+    # p.para_merge_sort(test_list)
+    # p2 = Parallel(0)
+    # p3 = Parallel(-1)
+    # p4 = Parallel(100)
 
 if __name__ == "__main__":
     main()
---
+++ src/parallel.py
@@ -0,0 +1,39 @@
+import multiprocessing as mp
+from multiprocessing import Process
+from src.merge_sort import merge_sort, merge
+
+
+class Parallel:
+    def __init__(self, n_workers: int, strategy='first_split'):
+        self.n_workers = n_workers
+        self.strategy = strategy
+        if n_workers > mp.cpu_count():
+            print(f'Number of workers specified is greater than number of cores. Using {mp.cpu_count()} workers')
+            self.pool = mp.Pool(mp.cpu_count())
+        elif n_workers <= 0:
+            print(f'Number of workers specified is invalid. Using {mp.cpu_count()} workers')
+            self.pool = mp.Pool(mp.cpu_count())
+        else:
+            print(f'Number of workers specified is {n_workers} workers')
+            self.pool = mp.Pool(n_workers)
+
+    def para_merge_sort(self, arr):
+        # TODO improve this
+        arrays = []
+        for i in range(self.n_workers):
+            arrays.append(arr[i::self.n_workers]) # split the array into n_workers parts
+        n_sorted_arr = self.pool.map(merge_sort, arrays)
+        sorted = self.para_merge(n_sorted_arr)
+        print(sorted)
+
+    def para_merge(self, n_sorted_arr):
+        # make 2 groups
+        if len(n_sorted_arr) > 2:
+            mid = len(n_sorted_arr) // 2
+            left = self.para_merge(n_sorted_arr[mid:])
+            right = self.para_merge(n_sorted_arr[:mid])
+            return merge(left, right, [0] * (len(left)+len(right)))
+        elif len(n_sorted_arr) == 1:
+            return n_sorted_arr[0]
+        else:
+            return merge(n_sorted_arr[0], n_sorted_arr[1], [0] * (len(n_sorted_arr[0])+len(n_sorted_arr[1])))
