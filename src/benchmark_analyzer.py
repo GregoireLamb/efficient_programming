363238db91c03de21592a609273817e87e3b5681
GregoireLamb <gdelambertye03@gmail.com>
2023-12-08 16:32:35 +0100

    fix and improve benchmark + improve parallel

--- src/benchmark_analyzer.py
+++ src/benchmark_analyzer.py
@@ -6,14 +6,17 @@ from datetime import datetime
 import pandas as pd
 from func_timeout import func_timeout, FunctionTimedOut
 
+import merge_sort
+
 import numpy as np
 import psutil
 import pickle
 
 
 class BenchmarkAnalyzer:
-    def __init__(self, sort_function: Callable[[List[int]], List[int]], first_n_lists: int = None):
+    def __init__(self, sort_function: Callable[[List[int]], List[int]], first_n_lists: int = None, baseline_function=merge_sort):
         self._sort_function = sort_function
+        self._baseline_function = baseline_function
         self._first_n_lists = first_n_lists
         self._benchmark_instances_path = '../instances/benchmark_instances.pkl'
 
@@ -51,7 +54,8 @@ class BenchmarkAnalyzer:
         df_custom = self.run_benchmark_for_method(instances, method_name=self._sort_function.__name__)
         self._sort_function = sorted
         df_sorted = self.run_benchmark_for_method(instances, method_name=f'py_sort')
-        df = pd.concat([df_custom, df_sorted])
+        df_baseline = self.run_benchmark_for_method(instances, method_name=self._baseline_function.__name__)
+        df = pd.concat([df_custom, df_sorted, df_baseline])
         # If results dir does not exist, create it
         if not os.path.exists('../results'):
             os.makedirs('../results')
@@ -64,42 +68,53 @@ class BenchmarkAnalyzer:
 
     def run_n_benchmarks(self, n=10):
         res = {
-            'elapsed_time': [],
-            'cpu_time': [],
-            'memory_usage': []
+            'elapsed_time_pysort': [],
+            'cpu_time_pysort': [],
+            'memory_usage_pysort': [],
+            'elapsed_time_baseline': [],
+            'cpu_time_baseline': [],
+            'memory_usage_baseline': []
         }
         for i in range(n):
             print(f'Running benchmark {i + 1}/{n}')
             final_avg = self.run_benchmark()
             print(f'Final average: {final_avg}')
-            res['elapsed_time'].append(final_avg['elapsed_time_rel_diff'])
-            res['cpu_time'].append(final_avg['cpu_time_rel_diff'])
-            res['memory_usage'].append(final_avg['memory_usage_rel_diff'])
+            res['elapsed_time_pysort'].append(final_avg['elapsed_time_rel_diff_pysort'])
+            res['cpu_time_pysort'].append(final_avg['cpu_time_rel_diff_pysort'])
+            res['memory_usage_pysort'].append(final_avg['memory_usage_rel_diff_pysort'])
+            res['elapsed_time_baseline'].append(final_avg['elapsed_time_rel_diff_baseline'])
+            res['cpu_time_baseline'].append(final_avg['cpu_time_rel_diff_baseline'])
+            res['memory_usage_baseline'].append(final_avg['memory_usage_rel_diff_baseline'])
 
         dashed_line = '-' * 50
         print(dashed_line)
-        print(f'Average of relative_elapsed_time: {np.mean(res["elapsed_time"])}')
-        print(f'Average of relative_cpu_time: {np.mean(res["cpu_time"])}')
-        print(f'Average of relative_memory_usage: {np.mean(res["memory_usage"])}')
+        print(f'Average of relative_elapsed_time:\n'
+              f'\tPySort {np.mean(res["elapsed_time_pysort"])}\n\tBaseline {np.mean(res["elapsed_time_baseline"])}')
+        print(f'Average of relative_cpu_time: \n\tPySort {np.mean(res["cpu_time_pysort"])} \n\tBaseline {np.mean(res["cpu_time_baseline"])}')
+        print(f'Average of relative_memory_usage: \n\tPySort {np.mean(res["memory_usage_pysort"])} \n\tBaseline {np.mean(res["memory_usage_baseline"])}')
 
-        print(f'Std of {n} relative_elapsed_time: {np.std(res["elapsed_time"])}')
-        print(f'Std of {n} relative_cpu_time: {np.std(res["cpu_time"])}')
-        print(f'Std of {n} relative_memory_usage: {np.std(res["memory_usage"])}')
+        print(f'Std of {n} relative_elapsed_time: \n\tPySort {np.std(res["elapsed_time_pysort"])} \n\tBaseline {np.std(res["elapsed_time_baseline"])}')
+        print(f'Std of {n} relative_cpu_time: \n\tPySort {np.std(res["cpu_time_pysort"])} \n\tBaseline {np.std(res["cpu_time_baseline"])}')
+        print(f'Std of {n} relative_memory_usage: \n\tPySort {np.std(res["memory_usage_pysort"])} \n\tBaseline {np.std(res["memory_usage_baseline"])}')
         print(dashed_line)
 
     @staticmethod
     def _print_final_results(df):
-        method_name = [a for a in df.method.unique() if a != 'py_sort'][0]
+        method_name = [a for a in df.method.unique() if a != 'py_sort' and a != "merge_sort"][0]
         not_finished = df[(df.elapsed_time.isna()) & (method_name == df.method)]
         if len(not_finished) > 0:
             print(f'{method_name} could not sort {not_finished}/{len(df[df.method == method_name])} instances')
 
         # Have method as a column for each instance, have (elapsed_time, cpu_time, memory_usage) as columns
         df = df.pivot(index='size', columns='method', values=['elapsed_time', 'cpu_time', 'memory_usage'])
-        # Compute the relative difference between the two methods
-        df['elapsed_time_rel_diff'] = df['elapsed_time'][method_name] / df['elapsed_time']['py_sort'] - 1
-        df['cpu_time_rel_diff'] = df['cpu_time'][method_name] / df['cpu_time']['py_sort'] - 1
-        df['memory_usage_rel_diff'] = df['memory_usage'][method_name] / df['memory_usage']['py_sort'] - 1
+        # Compute the relative difference with py_sort
+        df['elapsed_time_rel_diff_pysort'] = df['elapsed_time'][method_name] / df['elapsed_time']['py_sort'] - 1
+        df['cpu_time_rel_diff_pysort'] = df['cpu_time'][method_name] / df['cpu_time']['py_sort'] - 1
+        df['memory_usage_rel_diff_pysort'] = df['memory_usage'][method_name] / df['memory_usage']['py_sort'] - 1
+        # Compute the relative difference with baseline
+        df['elapsed_time_rel_diff_baseline'] = df['elapsed_time'][method_name] / df['elapsed_time']['merge_sort'] - 1
+        df['cpu_time_rel_diff_baseline'] = df['cpu_time'][method_name] / df['cpu_time']['merge_sort'] - 1
+        df['memory_usage_rel_diff_baseline'] = df['memory_usage'][method_name] / df['memory_usage']['merge_sort'] - 1
 
         # Print averages of relative differences
         dashed_line = '-' * 50
@@ -108,10 +123,15 @@ class BenchmarkAnalyzer:
         print(dashed_line)
         print(f'Average relative differences for method {method_name}')
 
-        final_averages = df[['elapsed_time_rel_diff', 'cpu_time_rel_diff', 'memory_usage_rel_diff']].mean(numeric_only=True)
-        print(f' -- Elapsed time: {final_averages["elapsed_time_rel_diff"]}')
-        print(f' -- CPU time: {final_averages["cpu_time_rel_diff"]}')
-        print(f' -- Memory usage: {final_averages["memory_usage_rel_diff"]}')
+        final_averages = (df[['elapsed_time_rel_diff_pysort', 'cpu_time_rel_diff_pysort', 'memory_usage_rel_diff_pysort',
+                             'elapsed_time_rel_diff_baseline', 'cpu_time_rel_diff_baseline', 'memory_usage_rel_diff_baseline']].mean(numeric_only=True))
+        print(f' -- Against     : PySort         Baseline')
+        print(
+            f' -- Elapsed time: {final_averages["elapsed_time_rel_diff_pysort"].item():.4f},        {final_averages["elapsed_time_rel_diff_baseline"].item():.4f}')
+        print(
+            f' -- CPU time    : {final_averages["cpu_time_rel_diff_pysort"].item():.4f},        {final_averages["cpu_time_rel_diff_baseline"].item():.4f}')
+        print(
+            f' -- Memory usage: {final_averages["memory_usage_rel_diff_pysort"].item():.4f},        {final_averages["memory_usage_rel_diff_baseline"].item():.4f}')
         print(dashed_line)
 
         return final_averages
@@ -134,7 +154,7 @@ class BenchmarkAnalyzer:
             try:
                 # Run the sort function with a timeout of 10 seconds
                 sorted_list = func_timeout(10, self._sort_function, args=(instance,))
-                assert sorted(instance) == sorted_list, f'Error in merge sort. Result is not sorted'
+                # assert sorted(instance) == sorted_list, f'Error in merge sort. Result is not sorted'
             except FunctionTimedOut:
                 print("Sort function exceeded time limit of 10 seconds.")
                 print(f" -- Elapsed time: TL")
--- src/parallel.py
+++ src/parallel.py
@@ -18,22 +18,15 @@ class Parallel:
             self.pool = mp.Pool(n_workers)
 
     def para_merge_sort(self, arr):
-        # TODO improve this
-        arrays = []
-        for i in range(self.n_workers):
-            arrays.append(arr[i::self.n_workers]) # split the array into n_workers parts
-        n_sorted_arr = self.pool.map(merge_sort, arrays)
-        sorted_list = self.para_merge(n_sorted_arr)
-        return sorted_list
+        arrays = [arr[i::self.n_workers] for i in range(self.n_workers)]  # split the array into n_workers parts
+        return self.para_merge(self.pool.map(merge_sort, arrays))
 
     def para_merge(self, n_sorted_arr):
-        # make 2 groups
-        if len(n_sorted_arr) > 2:
-            mid = len(n_sorted_arr) // 2
-            left = self.para_merge(n_sorted_arr[mid:])
-            right = self.para_merge(n_sorted_arr[:mid])
-            return merge(left, right, [0] * (len(left)+len(right)))
-        elif len(n_sorted_arr) == 1:
-            return n_sorted_arr[0]
-        else:
-            return merge(n_sorted_arr[0], n_sorted_arr[1], [0] * (len(n_sorted_arr[0])+len(n_sorted_arr[1])))
+        if len(n_sorted_arr) <= 1:
+            return n_sorted_arr[0] if n_sorted_arr else []
+
+        mid = len(n_sorted_arr) // 2
+        left = self.para_merge(n_sorted_arr[:mid])
+        right = self.para_merge(n_sorted_arr[mid:])
+
+        return merge(left, right, [0] * (len(left) + len(right)))
